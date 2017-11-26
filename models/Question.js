0b46eda19f4faee9697ba7f379a2357665342841
Esther Liao <erliao@princeton.edu>
2017-11-26 20:06:00 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -5,7 +5,9 @@ const db = require("../config/db")
 class Question{
   static CreateTable() {
     return new Promise(function(resolve){
-      resolve("This Does Nothing!")
+      const sql = `CREATE TABLE questions (id INTEGER PRIMARY KEY)`
+      db.run(sql, function(){
+      resolve("questions table created")
     })
   }
 }
